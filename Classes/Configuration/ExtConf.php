d2ece3684ad9bd53586621f8c5b520203c70e085
Stefan Froemken <froemken@gmail.com>
2021-02-04 10:30:33 +0100

    Add extConf for access_token

---
+++ Classes/Configuration/ExtConf.php
@@ -0,0 +1,53 @@
+<?php
+
+declare(strict_types=1);
+
+/*
+ * This file is part of the package jweiland/walls-io-proxy.
+ *
+ * For the full copyright and license information, please read the
+ * LICENSE file that was distributed with this source code.
+ */
+
+namespace JWeiland\WallsIoProxy\Configuration;
+
+use TYPO3\CMS\Core\SingletonInterface;
+
+/**
+ * This class will streamline the values from extension manager configuration
+ */
+class ExtConf implements SingletonInterface
+{
+    /**
+     * @var string
+     */
+    protected $accessToken = '';
+
+    public function __construct()
+    {
+        // On a fresh installation this value can be null.
+        if (isset($GLOBALS['TYPO3_CONF_VARS']['EXT']['extConf']['walls_io_proxy'])) {
+            // get global configuration
+            $extConf = unserialize($GLOBALS['TYPO3_CONF_VARS']['EXT']['extConf']['walls_io_proxy']);
+            if (is_array($extConf)) {
+                // call setter method foreach configuration entry
+                foreach ($extConf as $key => $value) {
+                    $methodName = 'set' . ucfirst($key);
+                    if (method_exists($this, $methodName)) {
+                        $this->$methodName($value);
+                    }
+                }
+            }
+        }
+    }
+
+    public function getAccessToken(): string
+    {
+        return $this->accessToken;
+    }
+
+    public function setAccessToken(string $accessToken): void
+    {
+        $this->accessToken = $accessToken;
+    }
+}
