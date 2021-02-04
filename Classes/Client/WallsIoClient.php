d56d7461e97715cd419b796b73606665fb047b42
Stefan Froemken <froemken@gmail.com>
2021-02-04 12:40:46 +0100

    Use walls.io API instead of Socket

--- Classes/Client/WallsIoClient.php
+++ Classes/Client/WallsIoClient.php
@@ -11,11 +11,11 @@ declare(strict_types=1);
 
 namespace JWeiland\WallsIoProxy\Client;
 
-use JWeiland\WallsIoProxy\Service\WallsService;
-use Ratchet\Client\WebSocket;
-use React\EventLoop\Factory;
-use React\Socket\Connector;
+use JWeiland\WallsIoProxy\Client\Request\RequestInterface;
+use JWeiland\WallsIoProxy\Helper\MessageHelper;
+use TYPO3\CMS\Core\Messaging\FlashMessage;
 use TYPO3\CMS\Core\Utility\GeneralUtility;
+use TYPO3\CMS\Extbase\Utility\LocalizationUtility;
 
 /**
  * This is the walls.io client which will send the request to the walls.io server
@@ -23,142 +23,92 @@ use TYPO3\CMS\Core\Utility\GeneralUtility;
 class WallsIoClient
 {
     /**
-     * @var string
+     * @var MessageHelper
      */
-    protected $wallsUri = 'wss://broadcaster.walls.io:443/socket.io/';
+    protected $messageHelper;
+
+    public function __construct(MessageHelper $messageHelper = null)
+    {
+        $this->messageHelper = $messageHelper ?? GeneralUtility::makeInstance(MessageHelper::class);
+    }
+
+    public function processRequest(RequestInterface $request): array
+    {
+        if (!$request->isValidRequest()) {
+            $this->messageHelper->addFlashMessage(
+                'URI is empty or contains invalid chars. URI: ' . $request->buildUri(),
+                'Invalid request URI',
+                FlashMessage::ERROR
+            );
+            return [];
+        }
+
+        $processedResponse = [];
+        $clientReport = [];
+        $response = GeneralUtility::getUrl($request->buildUri(), 0, null, $clientReport);
+        $this->checkClientReportForErrors($clientReport);
+        if (!$this->hasErrors()) {
+            $processedResponse = json_decode($response, true);
+            $this->checkResponseForErrors($processedResponse);
+        }
+
+        if ($this->hasErrors()) {
+            $processedResponse = [];
+        }
+
+        return $processedResponse;
+    }
+
+    public function hasErrors(): bool
+    {
+        return $this->messageHelper->hasErrorMessages();
+    }
 
     /**
-     * @var WallsService
+     * @return FlashMessage[]
      */
-    protected $wallsService;
+    public function getErrors(): array
+    {
+        return $this->messageHelper->getErrorMessages();
+    }
 
     /**
-     * @var array
+     * This method will only check the report of the client and not the result itself.
+     *
+     * @param array $clientReport
      */
-    protected $error = [];
-
-    public function __construct(WallsService $wallsService)
+    protected function checkClientReportForErrors(array $clientReport)
     {
-        $this->wallsService = $wallsService;
-    }
-
-    public function processRequest(WallsIoRequest $request): WallsIoResponse
-    {
-        $this->error = [];
-        $wallsIoResponse = GeneralUtility::makeInstance(WallsIoResponse::class);
-
-        if (!$this->isValidRequest($request)) {
-            return $wallsIoResponse;
+        if (!empty($clientReport['message'])) {
+            $this->messageHelper->addFlashMessage(
+                $clientReport['message'],
+                $clientReport['title'],
+                $clientReport['severity']
+            );
         }
-
-        $loop = Factory::create();
-        $reactConnector = new Connector(
-            $loop,
-            [
-                'dns' => '8.8.8.8',
-                'timeout' => 6,
-            ]
-        );
-
-        $connector = new \Ratchet\Client\Connector($loop, $reactConnector);
-        $connector($this->buildWallsUri($request), ['protocol1', 'subprotocol2'], $this->getHeaders())
-            ->then(function (WebSocket $conn) use ($wallsIoResponse) {
-                $conn->on('message', function (\Ratchet\RFC6455\Messaging\MessageInterface $msg) use ($conn, $wallsIoResponse) {
-                    $matches = [];
-                    if (preg_match('/(?<json>[\{|\[].*[\]|\}])/', $msg->getContents(), $matches)) {
-                        if (isset($matches['json']) && !empty($matches['json'])) {
-                            $data = json_decode($matches['json'], true);
-                            if (!empty($data)) {
-                                if (array_key_exists(0, $data) && $data[0] === 'new checkins') {
-                                    $wallsIoResponse->setBody($matches['json']);
-                                    $conn->close();
-                                }
-                            }
-                        }
-                    }
-                });
-            }, function (\Exception $e) use ($loop) {
-                $this->error = [
-                    'message' => 'An error occurred while retrieving data from walls.io',
-                    'title' => 'Error from walls.io: ' . $e->getMessage()
-                ];
-                $loop->stop();
-            });
-
-        $loop->run();
-
-        return $wallsIoResponse;
     }
 
-    protected function isValidRequest(WallsIoRequest $request): bool
+    /**
+     * Check processed response from Google Maps Server for errors
+     *
+     * @param array|null $response
+     */
+    protected function checkResponseForErrors($response)
     {
-        if (!$request->getWallId()) {
-            $this->error = [
-                'message' => 'You have forgotten to define a Wall ID in Plugin Configuration',
-                'title' => 'Missing mandatory Wall ID in Request'
-            ];
-            return false;
+        if ($response === null) {
+            $this->messageHelper->addFlashMessage(
+                'The response of walls.io was not a valid JSON response.',
+                'Invalid JSON response',
+                FlashMessage::ERROR
+            );
+        } elseif ($response['status'] !== 'success') {
+            // SF: Haven't found an error request as example.
+            // Correct following line, if you get one ;-)
+            $this->messageHelper->addFlashMessage(
+                implode($response['info']),
+                $response['status'],
+                FlashMessage::ERROR
+            );
         }
-        return true;
-    }
-
-    public function hasError(): bool
-    {
-        return !empty($this->error['message']);
-    }
-
-    public function getError(): array
-    {
-        if (!empty($this->error['message'])) {
-            return $this->error;
-        }
-        return [];
-    }
-
-    protected function buildWallsUri(WallsIoRequest $request): string
-    {
-        return rtrim($this->wallsUri, '?/') . '/?' . http_build_query($this->getWallsUriParameters($request));
-    }
-
-    protected function getWallsUriParameters(WallsIoRequest $request): array
-    {
-        $wallsUriParameters = [
-            'wallId' => (string)$request->getWallId(),
-            'client' => 'wallsio-frontend',
-            'network' => '',
-            'EIO' => '3',
-            'transport' => 'websocket',
-            't' => $this->getTimestamp()
-        ];
-
-        if ($request->getEntriesToLoad()) {
-            $wallsUriParameters['initialCheckins'] = (string)$request->getEntriesToLoad();
-        }
-
-        if ($request->getUseBinarySupport() === false) {
-            $wallsUriParameters['b64'] = '1';
-        }
-
-        return $wallsUriParameters;
-    }
-
-    public function getTimestamp(): string
-    {
-        return $this->wallsService->getFormattedTimestamp((int)date('U'));
-    }
-
-    protected function getHeaders(): array
-    {
-        $headers = [
-            'accept' => '*/*',
-            'accept-encoding' => 'gzip, deflate, br',
-            'accept-language' =>'de-DE,de;q=0.9,en-US;q=0.8,en;q=0.7,so;q=0.6',
-            'cache-control' => 'no-cache',
-            'dnt' => '1',
-            'pragma' => 'no-cache',
-            'user-agent' => 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_14_6) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/79.0.3945.117 Safari/537.36'
-        ];
-
-        return $headers;
     }
 }
---
+++ Classes/Helper/MessageHelper.php
@@ -0,0 +1,158 @@
+<?php
+
+declare(strict_types=1);
+
+/*
+ * This file is part of the package jweiland/walls-io-proxy.
+ *
+ * For the full copyright and license information, please read the
+ * LICENSE file that was distributed with this source code.
+ */
+
+namespace JWeiland\WallsIoProxy\Helper;
+
+use TYPO3\CMS\Core\Messaging\FlashMessage;
+use TYPO3\CMS\Core\Messaging\FlashMessageQueue;
+use TYPO3\CMS\Core\Messaging\FlashMessageService;
+use TYPO3\CMS\Core\Utility\GeneralUtility;
+
+/**
+ * Contains methods to create FlashMessage.
+ * Further we will implement a central position for Logging
+ */
+class MessageHelper
+{
+    /**
+     * @var FlashMessageService
+     */
+    protected $flashMessageService;
+
+    public function __construct(FlashMessageService $flashMessageService = null)
+    {
+        $this->flashMessageService = $flashMessageService ?? GeneralUtility::makeInstance(FlashMessageService::class);
+    }
+
+    /**
+     * @param string $message
+     * @param string $title
+     * @param int $severity
+     */
+    public function addFlashMessage(string $message, string $title = '', int $severity = FlashMessage::OK)
+    {
+        // We activate storeInSession, so that messages can be displayed when click on Save&Close button.
+        $flashMessage = GeneralUtility::makeInstance(
+            FlashMessage::class,
+            $message,
+            $title,
+            $severity,
+            true
+        );
+
+        $this->getFlashMessageQueue()->enqueue($flashMessage);
+    }
+
+    /**
+     * @param bool $flush
+     * @return FlashMessage[]
+     */
+    public function getAllFlashMessages($flush = true): array
+    {
+        if ($flush) {
+            return $this->getFlashMessageQueue()->getAllMessagesAndFlush();
+        }
+        return $this->getFlashMessageQueue()->getAllMessages();
+    }
+
+    public function hasMessages()
+    {
+        return !empty($this->getAllFlashMessages(false));
+    }
+
+    /**
+     * @param int $severity Must be one of the constants in AbstractMessage class
+     * @return FlashMessage[]
+     */
+    protected function getFlashMessagesBySeverity(int $severity): array
+    {
+        return $this->getFlashMessageQueue()->getAllMessages($severity);
+    }
+
+    /**
+     * @param int $severity Must be one of the constants in AbstractMessage class
+     * @return FlashMessage[]
+     */
+    public function getFlashMessagesBySeverityAndFlush(int $severity): array
+    {
+        return $this->getFlashMessageQueue()->getAllMessagesAndFlush($severity);
+    }
+
+    public function hasErrorMessages()
+    {
+        return !empty($this->getErrorMessages(false));
+    }
+
+    public function getErrorMessages(bool $flush = true): array
+    {
+        if ($flush) {
+            return $this->getFlashMessagesBySeverityAndFlush(FlashMessage::ERROR);
+        }
+        return $this->getFlashMessagesBySeverity(FlashMessage::ERROR);
+    }
+
+    public function hasWarningMessages()
+    {
+        return !empty($this->getWarningMessages(false));
+    }
+
+    public function getWarningMessages(bool $flush = true): array
+    {
+        if ($flush) {
+            return $this->getFlashMessagesBySeverityAndFlush(FlashMessage::WARNING);
+        }
+        return $this->getFlashMessagesBySeverity(FlashMessage::WARNING);
+    }
+
+    public function hasOkMessages()
+    {
+        return !empty($this->getOkMessages(false));
+    }
+
+    public function getOkMessages(bool $flush = true): array
+    {
+        if ($flush) {
+            return $this->getFlashMessagesBySeverityAndFlush(FlashMessage::OK);
+        }
+        return $this->getFlashMessagesBySeverity(FlashMessage::OK);
+    }
+
+    public function hasInfoMessages()
+    {
+        return !empty($this->getInfoMessages(false));
+    }
+
+    public function getInfoMessages(bool $flush = true): array
+    {
+        if ($flush) {
+            return $this->getFlashMessagesBySeverityAndFlush(FlashMessage::INFO);
+        }
+        return $this->getFlashMessagesBySeverity(FlashMessage::INFO);
+    }
+
+    public function hasNoticeMessages()
+    {
+        return !empty($this->getNoticeMessages(false));
+    }
+
+    public function getNoticeMessages(bool $flush = true): array
+    {
+        if ($flush) {
+            return $this->getFlashMessagesBySeverityAndFlush(FlashMessage::NOTICE);
+        }
+        return $this->getFlashMessagesBySeverity(FlashMessage::NOTICE);
+    }
+
+    protected function getFlashMessageQueue(): FlashMessageQueue
+    {
+        return $this->flashMessageService->getMessageQueueByIdentifier();
+    }
+}
