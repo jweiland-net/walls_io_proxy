75a6bc01347f40d26e491b326d5a67800fa2a114
Hoja Mustaffa Abdul Latheef <hlatheef@jweiland.net>
2024-07-31 12:40:12 +0200

    [TASK] Author details and minimum compatibility details updated

--- ext_emconf.php
+++ ext_emconf.php
@@ -4,14 +4,14 @@ $EM_CONF[$_EXTKEY] = [
     'title' => 'Walls.io Proxy',
     'description' => 'Cache and Proxy for walls.io, so no Cookie will be set on Client',
     'category' => 'plugin',
-    'author' => 'Stefan Froemken',
+    'author' => 'Stefan Froemken, Hoja Mustaffa Abdul Latheef',
     'author_email' => 'projects@jweiland.net',
     'author_company' => 'jweiland.net',
     'state' => 'stable',
-    'version' => '6.0.0',
+    'version' => '7.0.0',
     'constraints' => [
         'depends' => [
-            'typo3' => '10.4.36-11.5.99',
+            'typo3' => '12.4.17-12.4.99',
         ],
         'conflicts' => [
         ],
