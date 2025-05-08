a61452c3f874f3b78554f7477d7ba59eb5440298
Hoja Mustaffa Abdul Latheef <hlatheef@jweiland.net>
2025-05-08 13:36:35 +0200

    [TASK] Replaced FQCA with namespace class names

--- ext_tables.php
+++ ext_tables.php
@@ -1,5 +1,7 @@
 <?php
 
+use TYPO3\CMS\Core\Utility\ExtensionManagementUtility;
+
 /*
  * This file is part of the package jweiland/walls-io-proxy.
  *
@@ -11,7 +13,7 @@ if (!defined('TYPO3')) {
     die('Access denied.');
 }
 
-\TYPO3\CMS\Core\Utility\ExtensionManagementUtility::addLLrefForTCAdescr(
+ExtensionManagementUtility::addLLrefForTCAdescr(
     'tt_content.pi_flexform.wallsioproxy',
     'EXT:walls_io_proxy/Resources/Private/Language/locallang_csh_flexform.xlf'
 );
