a5d9178671c2b66072233268acc32c2c15546d3e
Hoja Mustaffa Abdul Latheef <hlatheef@jweiland.net>
2025-05-14 14:25:53 +0200

    [BUGFIX] Fixed Bug with ViewHelper

--- Classes/ViewHelpers/Be/Security/IsAdministratorViewHelper.php
+++ Classes/ViewHelpers/Be/Security/IsAdministratorViewHelper.php
@@ -25,14 +25,15 @@ class IsAdministratorViewHelper extends AbstractConditionViewHelper
     /**
      * This method decides, if the current logged-in user is an administrator
      *
-     * @param mixed $arguments ViewHelper arguments to evaluate the condition for this ViewHelper,
-     * allows for flexibility in overriding this method.
-     *
-     * @return bool Returns true if the user is an administrator, otherwise false.
+     * @return mixed Returns string if the user is an administrator, otherwise false.
      */
-    protected static function evaluateCondition($arguments = null): bool
+    public function render(): mixed
     {
-        return self::isBeUserAdmin();
+        if (self::isBeUserAdmin()) {
+            return $this->renderThenChild();
+        } else {
+            return false;
+        }
     }
 
     private static function isBeUserAdmin(): bool
